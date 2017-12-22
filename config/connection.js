beb5975f99896a21b3f8e4b93d68e0a720917b81
Vincent Huynh <vincenthuynh@Vincents-MacBook-Pro.local>
2017-12-22 12:08:12 -0600

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 var mysql = require('mysql');
 
 // Create the MySQL connection object
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
 		port: 3306,
-		host: 'us-cdbr-iron-east-05.cleardb.net',
-		user: 'b012c44ccb24fc',
-		password: '499421a8',
-		database: 'heroku_ba8ba00b4e65311'
-});
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'burgers_db'
+	})
+};
+
 
 
 
