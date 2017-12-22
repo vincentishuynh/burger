512e451ccdb3ebc8e023d727a260dc9925dad771
Vincent Huynh <vincenthuynh@Vincents-MacBook-Pro.local>
2017-12-22 11:07:51 -0600

    new changes

--- config/connection.js
+++ config/connection.js
@@ -2,29 +2,24 @@
 var mysql = require('mysql');
 
 // Create the MySQL connection object
-var connection;
-
-if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	// DB is local on localhost
-	connection = mysql.createConnection({
+var connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
 		user: 'root',
 		password: 'root',
 		database: 'burgers_db'
-	})
-};
+});
+
+
+
 
 // Make the connection to MySQL
 connection.connect(function(err) {
   if (err) {
-    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for ORM use
